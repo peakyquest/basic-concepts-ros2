880e8e4c7a93482ac5aed23bd7f99fd523b17705
peakyquest <haroon300@hotmail.com>
2024-10-01 20:09:54 +0500

    StrightLinePlugin Execution

--- path_planner_server/setup.py
+++ path_planner_server/setup.py
@@ -16,6 +16,7 @@ setup(
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         (os.path.join('share', package_name, 'rviz_config'), glob('rviz_config/*')),
+        (os.path.join('share', package_name, 'config/straightline_planner'), glob('config/straightline_planner/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
